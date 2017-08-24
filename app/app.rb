2124d353a68acd060ae01f331d2dc0ead566c44a
alessiobortone2 <alessiobortone2@gmail.com>
2017-08-24 15:54:31 +0100

    Step 15 completed

--- app/app.rb
+++ app/app.rb
@@ -1,12 +1,7 @@
 ENV['RACK_ENV'] ||= 'development'
-require 'sinatra'
-require_relative './models/link'
-require_relative './models/tag'
+
 require 'sinatra/base'
-
-
-
-
+require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
   get'/links' do
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -7,7 +7,3 @@ class Tag
   property :tagname, String
   has n, :links, :through => Resource
 end
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-DataMapper.finalize
-DataMapper.auto_upgrade!
